676858731d146770c9486d99abee7332541fd230
Jose Cordero <Jose Cordero@PEPE2>
2021-05-07 13:44:59 -0500

    primerCommit

---
+++ pruebaproyecto/src/pruebaproyecto/Pruebaproyecto.java
@@ -0,0 +1,15 @@
+
+package pruebaproyecto;
+
+
+public class Pruebaproyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //este es el primer commit
+    }
+    
+}
